d0b76b147ef3fd2db6d4dcf65f95573f3fc504b6
Daniel Werdermann <Daniel.Werdermann@teufel.de>
2016-01-27 10:20:00 +0100

    build fixes for linus darwin

--- service/execution_unix.go
+++ service/execution_unix.go
@@ -1,4 +1,4 @@
-// +build linux,darwin
+// +build linux darwin
 
 package service
 
@@ -6,21 +6,20 @@ import (
 	"os/user"
 	"syscall"
 	"os/exec"
-	"strings"
 	"github.com/echocat/caretakerd/values"
 	"github.com/echocat/caretakerd/panics"
 )
 
 func serviceHandleUsersFor(service *Service, cmd *exec.Cmd) {
 	config := (*service).config
-	u := config.User
-	if len(strings.TrimSpace(u)) > 0 {
+	userName := config.User
+	if !userName.IsTrimmedEmpty() {
 		cmd.SysProcAttr = &syscall.SysProcAttr{}
-		u, err := user.Lookup(u)
+		userId, err := user.Lookup(userName.String())
 		if err != nil {
-			panics.New("Could not run as user '%s'.", u).CausedBy(err).Throw()
+			panics.New("Could not run as user '%v'.", userName).CausedBy(err).Throw()
 		}
-		cmd.SysProcAttr.Credential = &syscall.Credential{Uid: u.Uid, Gid: u.Gid}
+		cmd.SysProcAttr.Credential = &syscall.Credential{Uid: userId.Uid, Gid: userId.Gid}
 	}
 }
 
