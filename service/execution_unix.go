14a63f60fb1d25298a189467a8bb2c22f41705fc
Daniel Werdermann <Daniel.Werdermann@teufel.de>
2016-01-27 11:02:38 +0100

    fix unix user lookup

--- service/execution_unix.go
+++ service/execution_unix.go
@@ -5,25 +5,45 @@ package service
 import (
 	"github.com/echocat/caretakerd/panics"
 	"github.com/echocat/caretakerd/values"
+	"os"
 	"os/exec"
 	"os/user"
+	"strconv"
 	"syscall"
 )
+
+func lookupUser(username string) (uid, gid int, err error) {
+	u, err := user.Lookup(username)
+	if err != nil {
+		return -1, -1, err
+	}
+	uid, err = strconv.Atoi(u.Uid)
+	if err != nil {
+		return -1, -1, err
+	}
+	gid, err = strconv.Atoi(u.Gid)
+	if err != nil {
+		return -1, -1, err
+	}
+	return uid, gid, nil
+}
 
 func serviceHandleUsersFor(service *Service, cmd *exec.Cmd) {
 	config := (*service).config
 	userName := config.User
 	if !userName.IsTrimmedEmpty() {
 		cmd.SysProcAttr = &syscall.SysProcAttr{}
-		userId, err := user.Lookup(userName.String())
+		//		u, err := userLookup(userName.String())
+		uid, gid, err := lookupUser(userName.String())
 		if err != nil {
 			panics.New("Could not run as user '%v'.", userName).CausedBy(err).Throw()
 		}
-		cmd.SysProcAttr.Credential = &syscall.Credential{Uid: userId.Uid, Gid: userId.Gid}
+		cmd.SysProcAttr.Credential = &syscall.Credential{Uid: uint32(uid), Gid: uint32(gid)}
 	}
 }
 
 func sendSignalToService(service *Service, process *os.Process, what values.Signal) error {
-	signal.RecordSendSignal(what)
+	values.RecordSendSignal(what)
 	process.Signal(syscall.Signal(what))
+	return nil
 }
