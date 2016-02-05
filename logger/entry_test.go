ea720d41786ad6c953795644a3c3a91e28566037
Gregor Noczinski <gregor@noczinski.eu>
2016-02-06 00:35:42 +0100

    * Fixed test

--- logger/entry_test.go
+++ logger/entry_test.go
@@ -12,11 +12,11 @@ func TestEntry_Format(t *testing.T) {
 		nil,
 		"My.Cool.Logger",
 		Info,
-		time.Unix(1452347970, 0),
+		time.Unix(1452347970, 0).UTC(),
 		"This is a test!",
 		time.Duration(10*time.Second),
 	)
 	f, err := e.Format("%d{YYYY-MM-DD HH:mm:ss}/%r [%-5.5p] [%-8.8c{2}] %m at %C{1}.%M(%F{1}:%L)%n", 0)
 	assert.Nil(t, err)
-	assert.Equal(t, "2016-01-09 14:59:30/10000 [INFO ] [Cool.Log] This is a test! at logger.TestEntry_Format(entry_test.go:10)\n", f)
+	assert.Equal(t, "2016-01-09 13:59:30/10000 [INFO ] [Cool.Log] This is a test! at logger.TestEntry_Format(entry_test.go:10)\n", f)
 }
