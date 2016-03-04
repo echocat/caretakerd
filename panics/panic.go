f57815c88b985e63e1c3424e02e58bd509712924
Gregor Noczinski <gregor@noczinski.eu>
2016-03-04 21:40:41 +0100

    * Cleanup tasks

--- panics/panic.go
+++ panics/panic.go
@@ -7,12 +7,14 @@ import (
 	"reflect"
 )
 
+// Panic represents a go panic which could have a cause and a call stack.
 type Panic struct {
 	message string
 	cause   interface{}
 	stack   stack.Stack
 }
 
+// New creates a new Panic instance with given message.
 func New(message string, a ...interface{}) Panic {
 	var targetMessage string
 	if len(a) == 0 {
@@ -35,27 +37,36 @@ func (i Panic) Error() string {
 	return i.Message()
 }
 
+// Message returns the message of this panic.
 func (i Panic) Message() string {
 	return i.message
 }
 
+// Cause returns the cause of this panic.
+// If the result is nil there is no cause.
 func (i Panic) Cause() interface{} {
 	return i.cause
 }
 
+// Stack returns the call stack of this panic.
 func (i Panic) Stack() stack.Stack {
 	return i.stack
 }
 
+// CausedBy is a shortcut for Stack() for using in builder pattern.
 func (i Panic) CausedBy(what interface{}) Panic {
 	i.cause = what
 	return i
 }
 
+// Throw throws this panic.
 func (i Panic) Throw() {
 	panic(i)
 }
 
+// DefaultPanicHandler could be used as panic handler on top method like
+//     defer panics.DefaultPanicHandler()
+// ...to handle every panic in a nicer way.
 func DefaultPanicHandler() {
 	if r := recover(); r != nil {
 		stack.Print(r, os.Stderr, 4)
@@ -63,6 +74,9 @@ func DefaultPanicHandler() {
 	}
 }
 
+// HandlePanicOfPresent could be used as panic handler on top method like
+//     defer panics.HandlePanicOfPresent(2)
+// ...to handle every panic in a nicer way.
 func HandlePanicOfPresent(framesToSkip int) {
 	if r := recover(); r != nil {
 		stack.Print(r, os.Stderr, 4+framesToSkip)
